a5797cb90a2f8a71f7f74c27f6b74cdbe484a59a
beibouyouhao <109951755+beibouyouhao@users.noreply.github.com>
2023-04-23 11:54:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=edc2f07e-266d-4672-be38-39b426bb2fcc
+AUUID=423e2833-83cf-4bc5-b2d4-16468582a532
 CADDYIndexPage=https://github.com/wulabing/3DCEList/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/beibouyouhao/meisoyoube/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/beibouyouhao/meisoyoube/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/kk3948/arsegugd/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/beibouyouhao/meisoyoube/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
